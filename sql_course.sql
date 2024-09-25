205a1ebb47a4bd4244fb4e29e3b3aec8b61a1023
Urochome <133230428+UrochomeRichy@users.noreply.github.com>
2024-09-25 15:31:50 +0300

    first commit

---
+++ sql_course.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE sql_course;
+
+--CREATE TABLE data_table (
+  --  jobID INT,
+    --job_name CHAR(50),
+    --Employee_name VARCHAR(100),
+    --saleSID INT
+--);
+
+--INSERT INTO (jobID, job_name, Employee_name, saleSID) VALUES
+--(0, Manager, Kyle Kihumba, 76),
+--(1, HR, Myles Alex, 56),
+--(2, Marketing director, Lavendar Humna, 46),
+--(3, Data Analyst, Kilwa Rishard, 49),
+--(4, Software engineer, Lyle Monaol, 79),
+--(5, Salesman, Alex Kamau, 58);
